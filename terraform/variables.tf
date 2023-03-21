d765247aed8c854a8f87e9ea7690124996d52e94
SSergeyA <93119897+SSergeyA@users.noreply.github.com>
2023-03-21 23:12:25 +0300

    Add files via upload

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "service_account_key" {
+  default     = ""
+}
+variable "cloud_id" {
+  type        = string
+  default     = "b1gd88frf016p6u7jbkb"
+}
+variable "folder_id" {
+  type        = string
+  default     = "b1gei3vd4ol9vg188m8e"
+}

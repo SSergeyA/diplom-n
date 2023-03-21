6346e8e294c6849df7675943c7928ba3a77531f6
SSergeyA <93119897+SSergeyA@users.noreply.github.com>
2023-03-21 23:09:07 +0300

    Delete Terraform directory

--- Terraform/provider.tf
+++
@@ -1,7 +0,0 @@
-provider "yandex" {
-  service_account_key_file = var.service_account_key
-  //service_account_key_file ="key.json"
-  cloud_id  = var.cloud_id
-  folder_id = var.folder_id
-  zone      = "ru-central1-a"
-}

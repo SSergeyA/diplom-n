32acdbd2fafb2a9155116de514eb7a5b43b0690e
SSergeyA <93119897+SSergeyA@users.noreply.github.com>
2023-03-24 12:50:23 +0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,3 +9,11 @@ variable "folder_id" {
   type        = string
   default     = "b1gei3vd4ol9vg188m8e"
 }
+variable "zone" {
+  type    = list(string)
+  default = ["ru-central1-a", "ru-central1-b", "ru-central1-c"]
+}
+variable "image_id" {
+  type        = string
+  default     = "fd89n8278rhueakslujo"
+}
